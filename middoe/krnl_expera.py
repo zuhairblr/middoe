9a431d4dd717493549c23336f1babb34fc71e34e
zuhairblr <zuhairtabrizi@gmail.com>
2025-05-23 18:58:10 +0200

    final polishing 22

--- middoe/krnl_expera.py
+++ middoe/krnl_expera.py
@@ -271,7 +271,7 @@ def _construct_par(model_name, theta_parameters):
         If parameters cannot be constructed or accessed.
     """
     try:
-        thetac = theta_parameters.get(model_name, [])
+        thetac = theta_parameters
         theta = [1] * len(thetac)
         return theta, thetac
     except Exception as e:
